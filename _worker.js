df7e51a3d0bab75416cb9b2957a6eb8e944513fe
Rabbie Wu <rabbie.cn@gmail.com>
2026-09-25 14:58:29 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6166,7 +6166,7 @@ async function 读取config_JSON(env, hostname, userID, UA = "Mozilla/5.0", 重�
 				指定端口: -1,
 			},
 			SUB: null,
-			SUBNAME: "edge" + "tunnel",
+			SUBNAME: "小莫面板",
 			SUBUpdateTime: 24, // 订阅更新时间（小时）
 			TOKEN: await MD5MD5(hostname + userID),
 		},
